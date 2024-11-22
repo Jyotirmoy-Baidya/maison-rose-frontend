22b09fab81b12c5d4c5356c3da4b188c294a597d
Jyotirmoy Baidya <jyotirmoybaidya408@gmail.com>
2024-11-23 01:40:48 +0530

    correction

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,16 @@ import { TiSocialFacebookCircular } from 'react-icons/ti'
 import { NavLink } from 'react-router-dom'
 import Menuarrow from '../components/cafe/Menuarrow'
 import Menuarrow2 from '../components/cafe/Menuarrow2'
-import { schedule } from '../constants/aboutus'
+
+const schedule = [
+    { day: "Monday", hours: "16:00 - 22:30" },
+    { day: "Tuesday", hours: "16:00 - 22:30" },
+    { day: "Wednesday", hours: "16:00 - 22:30" },
+    { day: "Thursday", hours: "16:00 - 22:30" },
+    { day: "Friday", hours: "16:00 - 22:30" },
+    { day: "Saturday", hours: "16:00 - 22:30" },
+    { day: "Sunday", hours: "16:00 - 22:30" },
+];
 
 
 const text = () => {
