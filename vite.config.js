066c37b13855cbdec5582d1ddb8f84407832e5e9
Jyotirmoy Baidya <jyotirmoybaidya408@gmail.com>
2024-11-24 14:13:45 +0530

    route changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import rewriteAll from 'vite-plugin-rewrite-all';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), rewriteAll()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  },
+});
